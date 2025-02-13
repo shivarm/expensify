00f3ed7cfcee3e754e583f45e5cc81f51e536d52
Shivam Sharma <meshivam81@gmail.com>
2025-02-13 19:48:15 +0530

    ensure SSL is enabled if require

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,10 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose
-      .connect(process.env.MONGO_URI)
-      .then(() => console.log('Connected to the database'))
-      .catch((error) => console.log('Error connecting to the database', error));
+    await mongoose.connect(process.env.MONGO_URI, {
+      ssl: true, // Ensure SSL is enabled if required
+    });
+    console.log('MongoDB connected');
   } catch (error) {
     console.log('Error connecting to the database', error);
     process.exit(1);
